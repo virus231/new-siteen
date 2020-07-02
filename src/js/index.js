13b747c3e5c8ebb9b5935d2ad608e192aa3c53d5
Virus <vladprodan6@gmail.com>
2020-07-02 14:37:57 +0300

    +++

--- src/js/index.js
+++ src/js/index.js
@@ -17,19 +17,16 @@ const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector("menu");
 const promo = document.querySelector(".promo-block");
 const links = document.querySelector(".nav-menu");
-const logo = document.querySelector(".logo");
-
-
+const main = document.querySelector("main");
+const sidebar = document.querySelector("sidebar");
 
 if (hamburger) {
   hamburger.onclick = () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
     promo.classList.toggle("active");
-    links.classList.toggle("active");
     logo.classList.toggle("active");
-
-
+    main.classList.toggle("active")
   }
 }
 
@@ -37,5 +34,45 @@ document.querySelectorAll("menu a").forEach((link) => {
   link.onclick = () => {
     hamburger.classList.remove("active");
     menu.classList.remove("active");
+    main.classList.remove("active");
   }
 });
+
+
+// Nav SM
+const hamburgerSM = document.querySelector(".hamburger-sm");
+const menuSM = document.querySelector("menuu");
+
+if (hamburgerSM) {
+  hamburgerSM.onclick = () => {
+    hamburgerSM.classList.toggle("active");
+    menuSM.classList.toggle("active");
+  }
+}
+
+document.querySelectorAll(".link-group-sm a").forEach((link) => {
+  link.onclick = () => {
+    hamburgerSM.classList.remove("active");
+    menuSM.classList.remove("active");
+  }
+});
+
+// Slider
+
+$(document).ready(function(){
+  $('.team-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false
+  });
+
+  $('button.prev').click(function(){
+    $(".team-slider").slick('slickPrev');
+  });
+
+  $('button.next').click(function(){
+    $(".team-slider").slick('slickNext');
+  });
+});
+
