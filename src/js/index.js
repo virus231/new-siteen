87fcbfb62f3e2e30d90268259f7c283f1938a080
Virus <vladprodan6@gmail.com>
2020-07-03 16:09:42 +0300

    +++

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var collapse_content_selector = $(this).attr('href');
     var toggle_switch = $(this);
     $(collapse_content_selector).toggle(function () {
-      if ($(this).css('display') == 'none') {
+      if ($(this).css('opacity') == '0') {
         toggle_switch.html('View More');
       } else {
         toggle_switch.html('View Less');
@@ -11,6 +11,7 @@ $(document).ready(function () {
     });
   });
 });
+
 // Your jQuery code
 
 const hamburger = document.querySelector(".hamburger");
@@ -18,18 +19,18 @@ const menu = document.querySelector("menu");
 const promo = document.querySelector(".promo-block");
 const links = document.querySelector(".nav-menu");
 const main = document.querySelector("main");
-const sidebar = document.querySelector("sidebar");
+const contactBlocks = document.querySelector(".contact-block");
 
 if (hamburger) {
   hamburger.onclick = () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
-    promo.classList.toggle("active");
-    logo.classList.toggle("active");
-    main.classList.toggle("active")
+    // promo.classList.toggle("active");
+    // logo.classList.toggle("active");
+    main.classList.toggle("active");
+
   }
 }
-
 document.querySelectorAll("menu a").forEach((link) => {
   link.onclick = () => {
     hamburger.classList.remove("active");
@@ -41,12 +42,14 @@ document.querySelectorAll("menu a").forEach((link) => {
 
 // Nav SM
 const hamburgerSM = document.querySelector(".hamburger-sm");
-const menuSM = document.querySelector("menuu");
+const menuSM = document.querySelector(".link-group-sm");
+// const mainSM = document.querySelector("main");
 
 if (hamburgerSM) {
   hamburgerSM.onclick = () => {
     hamburgerSM.classList.toggle("active");
     menuSM.classList.toggle("active");
+    // mainSM.classList.toggle("active")
   }
 }
 
@@ -59,20 +62,68 @@ document.querySelectorAll(".link-group-sm a").forEach((link) => {
 
 // Slider
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.team-slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 998,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
-  $('button.prev').click(function(){
+  $('button.prev').click(function () {
     $(".team-slider").slick('slickPrev');
   });
 
-  $('button.next').click(function(){
+  $('button.next').click(function () {
     $(".team-slider").slick('slickNext');
   });
 });
 
+
+// Adv
+var btn = document.querySelector('.btn-show');
+btn.onclick = function (e) {
+  var element = document.querySelector(".hidden-area");
+  if (element.classList.contains("show-block")) {
+    element.classList.remove("show-block");
+  } else {
+    element.classList.add("show-block");
+  }
+}
