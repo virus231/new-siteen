9ee2a0626296fd87114f8dd977dd1fb7f700bed3
Virus <vladprodan6@gmail.com>
2020-07-03 19:20:04 +0300

    +++

--- src/js/index.js
+++ src/js/index.js
@@ -88,8 +88,8 @@ $(document).ready(function () {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true
         }
       },
